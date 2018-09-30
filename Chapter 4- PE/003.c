8e038dc6330abf704e6fadb7db9a9d74349b7a0e
Hong Wei <teohongwei898@gmail.com>
2018-09-30 14:24:19 +0800

    Create 003.c

---
+++ Chapter 4- PE/003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float num;
+
+	printf("Enter a number: ");
+	scanf("%f", &num);
+	printf("The input is %.1f or %.1e\n", num, num);
+	printf("The input is %+.3f or %.3E\n", num, num);
+
+	return 0;
+}
