4c5eba705312807ca79fec9093cb5fa95dff36d7
Hong Wei <teohongwei898@gmail.com>
2018-09-30 13:41:02 +0800

    Create 003.c

---
+++ Chapter 3- PE/003.c
@@ -0,0 +1,10 @@
+/* Programming Exercise 3-3 */
+#include <stdio.h>
+int main(void)
+
+{
+	printf("\a"); // sound alert
+	printf("Startled by the sudden sound, Sally shouted, \n""By the Great Pumpkin, what was that!\"\n");
+
+	return 0;
+}
