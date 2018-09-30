34c3194722351854c12fc4b1b43984e6db37ad5a
Hong Wei <teohongwei898@gmail.com>
2018-09-30 12:46:48 +0800

    Create 001.c

---
+++ Chapter 2- PE/001.c
@@ -0,0 +1,12 @@
+/* Programming Exercise 2-1 */
+#include <stdio.h>
+int main(void)
+{
+ printf("Hong Wei\n");
+ printf("Hong\nWei\n");
+ printf("Hong");
+ printf("Wei\n");
+ return 0;
+}
+PE	2-3
+/* Programming 
