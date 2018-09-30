077cc1daf297ce54c940bfda3e8d649b22b84634
Hong Wei <teohongwei898@gmail.com>
2018-09-30 13:18:53 +0800

    Create 006.c

---
+++ Chapter 2- PE/006.c
@@ -0,0 +1,13 @@
+/* Programming Exercise 2-6 */
+#include <stdio.h>
+int main(void)
+{
+ int toes;
+ int toestw;
+ int toessq;
+ toes = 10;
+ toestw = 2 * toes;
+ toessq = toes * toes;
+ printf("Amount of toes is %d, twice of toes is %d, squared of toes is %d", toes, toestw, toessq);
+ return 0;
+}
