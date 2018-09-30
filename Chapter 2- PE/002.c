de9bfdce3faeb028f0c6893666bb7bec8dacc7de
Hong Wei <teohongwei898@gmail.com>
2018-09-30 13:00:11 +0800

    Create 002.c

---
+++ Chapter 2- PE/002.c
@@ -0,0 +1,10 @@
+/* Programming Exercise 2-2 */
+#include <stdio.h>
+int main(void)
+{
+ printf("Hong Wei\n");
+ printf("Block 123\nABC Street\n");
+ printf("123456\n ");
+ printf("#01-23\n");
+ return 0;
+ }
