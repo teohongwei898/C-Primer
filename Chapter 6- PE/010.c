57acf8178554943b0211125b4ac6dadb7ad3d209
Hong Wei <teohongwei898@gmail.com>
2018-10-01 14:01:53 +0800

    Create 010.c

---
+++ Chapter 6- PE/010.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sum_of_squares(int lower, int upper);
+
+int main(void)
+{
+	int upper, lower, reads;
+
+	printf("Enter lower and upper integer limits: ");
+	while(reads = scanf("%d%d", &lower, &upper), reads == 2 && lower < upper)
+	{
+		printf("The sums of the squares from %d to %d is %d\n",
+			   lower * lower, upper * upper, sum_of_squares(lower, upper));
+		printf("Enter next set of limits: ");
+	}
+	printf("Done\n");
+
+	return 0;
+}
+
+int sum_of_squares(int lower, int upper) // calculate sum of squares from lower to upper
+{
+	int sum = 0;
+
+	for (int i = lower; i <= upper; i++)
+	{
+		sum += i * i;
+	}
