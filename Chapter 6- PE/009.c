6c23a87f87094be57d2c4ed32995c345ce60427c
Hong Wei <teohongwei898@gmail.com>
2018-10-01 13:25:45 +0800

    Create 009.c

---
+++ Chapter 6- PE/009.c
@@ -0,0 +1,24 @@
+include <stdio.h>
+
+float calculate(float n1, float n2);
+
+int main(void)
+{
+	float num1, num2;
+	int reads;
+	
+	printf("Enter two floating-point numbers: ");
+	while (scanf(" %f %f", &num1, &num2) == 2)
+	{
+		printf("(%.3f - %.3f)/(%.3f * %.3f) = %.3f\n", num1, num2, num1, num2,
+		       calculate(num1, num2));
+		printf("Enter two floating-point numbers (enter non-numeric to quit): ");
+	}
+
+	return 0;
+}
+
+float calculate(float n1, float n2)
+{
+	return (n1 - n2) / (n1 * n2);
+}
