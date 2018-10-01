1282954e9b20262bfb1dff51c25f59fe86efdcbb
Hong Wei <teohongwei898@gmail.com>
2018-10-01 14:21:36 +0800

    Create 016.c

---
+++ Chapter 6- PE/016.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	const float DEIRDE_PRINCIPLE = 100.0f;
+	const float DAPHNE_PRINCIPLE = 100.0f;
+	const float DEIRDE_INTEREST = 0.05f;
+	const float DAPHNE_INTEREST = 0.10f;
+
+	// initialize years and balances
+	int years = 0; 
+	float daphne_balance = DAPHNE_PRINCIPLE;
+	float deirdre_balance = DEIRDE_PRINCIPLE;
+
+	while (deirdre_balance <= daphne_balance)
+	{
+		// eq. for compound interest
+		deirdre_balance *= 1.0f + DEIRDE_INTEREST;
+		// eq. for simple interest
+		daphne_balance += DAPHNE_PRINCIPLE * DAPHNE_INTEREST; 
+		years++;
+	}
+	printf("After %d years, Daphne's investment is worth $%.2f and "
+		   "Deirdre’s investment is worth $%.2f.\n", years,
+		   daphne_balance, deirdre_balance);
+
+	return 0;
+}
