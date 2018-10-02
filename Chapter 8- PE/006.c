4951d174d0c3d4535d9b4b5a334c666d82e87c16
Hong Wei <teohongwei898@gmail.com>
2018-10-02 11:43:26 +0800

    Create 006.c

---
+++ Chapter 8- PE/006.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int get_first(void);
+
+int main(void)
+{
+	int ch;
+
+	printf("Test program for get_first():\n");
+	printf("Enter a line; you should see the first non-whitespace\n");
+	printf("character echoed in the terminal:\n");
+
+	ch = get_first();
+	printf("%c\n", ch);
+
+	return 0;
+}
+
+int get_first(void)
+{
+	// returns first non-whitespace character and clears
+	// remaining input until next line break or EOF
+
+	int ch, garbage;
+
+	do {
+		ch = getchar();
+	}
+	while (isspace(ch));
+		
+
+	while((garbage = getchar()) != '\n' && garbage != EOF)
+		;
+
+	return ch;
+}
