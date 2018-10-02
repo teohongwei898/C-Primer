2903966539d160511b565a868a8db82fa7e48c1f
Hong Wei <teohongwei898@gmail.com>
2018-10-02 14:28:29 +0800

    Create 001.c

---
+++ Chapter 11- PE/001.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 20
+
+char * sgetnchar(char *array, int n);
+
+int main(void)
+{
+	// test sgetnchar
+
+	char hello[SIZE] = "Hello, ";
+	int space = SIZE - strlen(hello) - 1;
+
+	printf("What's your name? (enter %d characters)\n", space);
+	sgetnchar(hello + 7, space);
+	puts(hello);
+
+	return 0;
+}
+
+char * sgetnchar(char *array, int n)
+{
+	// gets n characters from input and stores them in character array
+
+	char ch;
+
+	for (int i = 0; i < n; i++)
+	{
+		if ((ch = getchar()) == EOF)
+			break;
+
+		*(array + i) = ch;
+	}
+
+	return array;
+}
