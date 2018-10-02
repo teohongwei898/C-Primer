a0543597f8858b78fae8a52d60dbbf9a7c09efb4
Hong Wei <teohongwei898@gmail.com>
2018-10-02 11:35:01 +0800

    Create 002.c

---
+++ Chapter 8- PE/002.c
@@ -0,0 +1,26 @@
+#int main(void)
+{
+	int ch, char_count = 0;
+
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch >= ' ')
+			printf("\'%c\': %d", ch, ch);
+		else if (ch == '\n')
+			printf("\'\\n\': %d", ch);
+		else if (ch == '\t')
+			printf("\'\\t\': %d", ch);
+		else // ascii control characters
+			printf("\'^%c\': %d", ch + 64, ch );
+
+		char_count++;
+		if (char_count % 10 == 0)
+			printf("\n"); // print new line for every 10 characters
+		else
+			printf("  "); // otherwise, print spaces
+	}
+
+	printf("\n");
+
+	return 0;
+}
