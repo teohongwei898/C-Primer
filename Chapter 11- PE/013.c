0d4851074e3a4f0868b53fa17b8d7508248f91d4
Hong Wei <teohongwei898@gmail.com>
2018-10-03 17:02:00 +0800

    Create 013.c

---
+++ Chapter 11- PE/013.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error: at least one command-line argument required.\n");
+		return 1;
+	}
+	else
+		for (int i = argc - 1; i > 0; i--)
+			printf("%s ", argv[i]);
+
+	puts("");
+	return 0;
+}
